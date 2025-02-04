385fdd9e15c434a71044a1603bd7c45de1d8b84a
Lincon Avila <lincon1000ls@gmail.com>
2025-02-04 17:44:41 -0300

    Add initial records to Publisher table

---
+++ library-database/publishers_insert.sql
@@ -0,0 +1,19 @@
+-- Publishers Table
+
+INSERT INTO Publisher (NamePublisher)
+VALUES
+('Prentice Hall'), ('O´Reilly'),
+('Aleph'), ('Microsoft Press'),  
+('Wiley'), ('HarperCollins'),  
+('Érica'), ('Novatec'),  
+('McGraw-Hill'), ('Apress'),  
+('Francisco Alves'), ('Sybex'),  
+('Globo'), ('Companhia das Letras'),  
+('Morro Branco'), ('Penguin Books'), ('Martin Claret'),  
+('Record'), ('Springer'), ('Melhoramentos'),  
+('Oxford'), ('Taschen'), ('Ediouro'), ('Bookman');
+
+
+--Testing
+SELECT * FROM Publisher
+ORDER BY IdPublisher;
