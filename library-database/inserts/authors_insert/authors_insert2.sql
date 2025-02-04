8ab65d239249fb4b368151cd2d4a49254c985e10
Lincon Avila <lincon1000ls@gmail.com>
2025-02-04 20:57:16 -0300

    Add more authors no authors table

---
+++ library-database/inserts/authors_insert/authors_insert2.sql
@@ -0,0 +1,5 @@
+INSERT INTO Author (NameAuthor, LastNameAuthor)
+VALUES
+('Herman', 'Melville'),('Jane', 'Austen'),('J.D.', 'Salinger'),
+('F. Scott', 'Fitzgerald'), ('Leo', 'Tolstoy'),('Fyodor', 'Dostoevsky'),('J.R.R.', 'Tolkien'),
+('Frank', 'Herbert'), ('Bram', 'Stoker'), ('Mary', 'Shelley');
