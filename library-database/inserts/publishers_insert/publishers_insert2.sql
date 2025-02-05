575344406b77182844d1df43a7d0fb3c8fdb601f
Lincon Avila <lincon1000ls@gmail.com>
2025-02-04 21:04:07 -0300

    Add more publishers to publishers table

---
+++ library-database/inserts/publishers_insert/publishers_insert2.sql
@@ -0,0 +1,9 @@
+INSERT INTO Publisher (NamePublisher)
+VALUES 
+    ('Scribner'),
+    ('Vintage'),
+    ('Houghton Mifflin Harcourt'),
+    ('Ace Books'),
+    ('Gollancz'),
+    ('Signet Classics'),
+    ('Lackington, Hughes, Harding, Mavor & Jones');
